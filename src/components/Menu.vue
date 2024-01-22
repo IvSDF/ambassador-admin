572ee5038643ea50d8711b787c50488e71fb2270
IvSDF <demchyshyn.ivan@gmail.com>
2024-01-22 21:13:16 +0200

    add_components

---
+++ src/components/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">
+            <span data-feather="home"></span>
+            Dashboard
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+import Nav from "@/components/Nav.vue";
+
+@Component({
+  components: {Nav}
+})
+export default class Menu extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
