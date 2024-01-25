4a79a87f31d25628451d4afa312aac9c4b7696a5
IvSDF <demchyshyn.ivan@gmail.com>
2024-01-25 22:45:49 +0200

    create_users_lists

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,25 +3,18 @@
     <div class="position-sticky pt-3">
       <ul class="nav flex-column">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">
-            <span data-feather="home"></span>
-            Dashboard
-          </a>
+          <router-link to="/users" class="nav-link" active-class="active">
+            Users
+          </router-link>
         </li>
       </ul>
     </div>
   </nav>
 </template>
 
-<script lang="ts">
-import {Vue, Component} from 'vue-property-decorator';
-import Nav from "@/components/Nav.vue";
-
-@Component({
-  components: {Nav}
-})
-export default class Menu extends Vue {
-
+<script>
+export default {
+  name: "Menu"
 }
 </script>
 
